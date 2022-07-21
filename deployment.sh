c44688ff3d0b6970068da7b1d3a42e44e0de99bc
otymkovskyi <otymkovskyi@playtika.com>
2022-07-21 15:12:58 +0200

    init

--- deployment.sh
+++ deployment.sh
@@ -3,6 +3,7 @@ docker build -t pet-project/config-server .
 docker tag pet-project/config-server registry.digitalocean.com/pet-project/config-server
 docker push registry.digitalocean.com/pet-project/config-server
 kubectl delete deploy config-server
+kubectl delete service config-server
 kubectl create -f kube-manifests/deployment.yaml
 kubectl get pods
 
