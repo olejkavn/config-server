13845a78bdb41aa74fe4fe4c54298d4affbf8c3e
otymkovskyi <otymkovskyi@playtika.com>
2022-08-10 16:29:17 +0200

    init

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,9 @@
 mvn clean install
-docker build -t pet-project/config-server .
-docker tag pet-project/config-server registry.digitalocean.com/pet-project/config-server
-docker push registry.digitalocean.com/pet-project/config-server
-kubectl delete deploy config-server
-kubectl delete service config-server
+docker build -t config-service .
+docker tag config-service otymkovskyi/config-service
+docker push otymkovskyi/config-service
+kubectl delete deploy config-service
+kubectl delete service config-service
 kubectl create -f kube-manifests/deployment.yaml
 kubectl get pods
 
